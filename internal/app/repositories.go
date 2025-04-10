c0ab3b973db5c8f912746408983d603c7c298c62
Sanchir01 <emgushovs@mail.ru>
2025-04-10 06:43:11 +0300

    add base structure

--- internal/app/repositories.go
+++ internal/app/repositories.go
@@ -1,8 +1,13 @@
 package app
 
+import "github.com/Sanchir01/golang-avito/internal/feature/user"
+
 type Repositories struct {
+	UserRepository *user.Repository
 }
 
 func NewRepositories(database *Database) *Repositories {
-	return &Repositories{}
+	return &Repositories{
+		UserRepository: user.NewRepository(database.PrimaryDB),
+	}
 }
--- internal/app/services.go
+++ internal/app/services.go
@@ -1,10 +1,13 @@
 package app
 
-import ()
+import "github.com/Sanchir01/golang-avito/internal/feature/user"
 
 type Services struct {
+	UserService *user.Service
 }
 
 func NewServices(r *Repositories, db *Database) *Services {
-	return &Services{}
+	return &Services{
+		UserService: user.NewService(r.UserRepository),
+	}
 }
